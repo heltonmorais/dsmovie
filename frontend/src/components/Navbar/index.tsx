e5f2f99e3e3c9321227e90c950288446a4c89bfb
Helton Morais <helton_hag@hotmail.com>
2022-03-07 21:28:21 -0300

    Correcao do estilo css no link do GitHub do Navbar

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -7,7 +7,7 @@ function Navbar() {
             <nav className="container">
                 <div className="dsmovie-nav-content">
                     <h1>DSMovie</h1>
-                    <a href="https://github.com/heltonmorais" target="_blank" rel="noreferrer">
+                    <a href="https://github.com/heltonmorais">
                         <div className="dsmovie-contact-container">
                             <GithubIcon />
                             <p className="dsmovie-contact-link">/heltonmorais</p>          
